a8b466d32b48ad02abc6be42234fc9c9e86ada7c
Horváth Viktor <karlsasse@gmail.com>
2021-04-29 10:29:25 +0200

    kész 2021.04.29.10:29

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,20 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+        Az adatbázis egyetlen táblát tartalmaz, dinosaur néven, az alábbi oszlopokkal:
+
+        breed varchar (elsődleges kulcs)
+        expected int
+        actual int
+        A checkOverpopulation metódus feladata, hogy térjen vissza azoknak a fajtáknak a nevével,
+        amiknél a valós létszám magasabb, mint az elvárt (a fenti példában a Maiasaurus,
+        a Velociraptor és a Hypsilophodontida), ABC szerint növekvő sorrendben.
+ */
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +27,20 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+    public List<String> checkOverpopulation() {
+        List<String> dinos = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String query = "SELECT breed FROM dinosaur where actual > expected ORDER BY breed;";
+            PreparedStatement st = connection.prepareStatement(query);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                dinos.add(rs.getString(1));
+            }
+            return dinos;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
